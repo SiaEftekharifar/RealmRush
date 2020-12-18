7c07c9c92cd0faedf893adec525ca36266877efe
Siavash Eftekharifar <siavash.eftekharifar1991@gmail.com>
2020-12-18 14:02:21 -0500

    AddedPositingStringToCube

--- RealmRush/Assets/CubeEditor.cs
+++ RealmRush/Assets/CubeEditor.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 
-public class EditorSnap : MonoBehaviour {
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField][Range(1f, 20f)] float gridSize;
+
+    TextMesh textMesh;
+
     
     void Update() {
 
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = (snapPos.x / gridSize).ToString() + "," + (snapPos.z/gridSize).ToString();
     }
 }
